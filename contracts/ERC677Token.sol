a4e8e9a33c28063bb07d4660edd542277ea39ead
Chris Hatch <hatch@tuta.io>
2019-10-29 12:21:17 +0700

    add NOTE about problem with workaround and solidity compiler bug

--- contracts/ERC677Token.sol
+++ contracts/ERC677Token.sol
@@ -17,7 +17,21 @@ contract ERC677Token is ERC677 {
     public
     returns (bool success)
   {
-    linkERC20Basic(this).transfer(_to, _value);
+    // This fails to compile due a Solidity compiler bug:
+    //   https://github.com/ethereum/solidity/issues/7558
+    // But it as actually what we need ... So restoring it for now.
+    //
+    // TODO: remove comment here and below once the compiler bug is fixed.
+    linkERC20Basic(super).transfer(_to, _value);
+
+    // Replacing the line above with the following line will make the
+    // contract compile however this is not what we want as it creates a
+    // fresh call and crucially sets msg.sender to the LinkToken address.
+    // Thus the transfer will not be from the caller of transferAndCall but
+    // from the LinkToken contract itself ...
+
+    // linkERC20Basic(this).transfer(_to, _value);
+
     emit Transfer(msg.sender, _to, _value, _data);
     if (isContract(_to)) {
       contractFallback(_to, _value, _data);
